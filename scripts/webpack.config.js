9c8117093d61a3e685d58993c2e446b5e190b3f6
shoonia <alexanderz@wix.com>
2021-04-24 13:50:17 +0300

    Setup Babel

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const createLocalIdent = require('mini-css-class-name/css-loader');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
@@ -148,12 +149,19 @@ module.exports = (webpackEnv) => {
               include: paths.appSrc,
               loader: require.resolve('babel-loader'),
               options: {
-                customize: require.resolve(
-                  'babel-preset-react-app/webpack-overrides'
-                ),
                 presets: [
+                  '@babel/preset-typescript',
                   [
-                    require.resolve('babel-preset-react-app'),
+                    '@babel/preset-env',
+                    {
+                      loose: true,
+                      browserslistEnv: webpackEnv,
+                      configPath: paths.appDirectory,
+                      useBuiltIns: 'entry',
+                    },
+                  ],
+                  [
+                    '@babel/preset-react',
                     {
                       runtime: 'automatic',
                     },
@@ -161,50 +169,18 @@ module.exports = (webpackEnv) => {
                 ],
                 plugins: [
                   isDev && require.resolve('react-refresh/babel'),
+                  [
+                    'babel-plugin-const-enum',
+                    {
+                      transform: 'constObject',
+                    },
+                  ],
                 ].filter(Boolean),
-                // This is a feature of `babel-loader` for webpack (not Babel itself).
-                // It enables caching results in ./node_modules/.cache/babel-loader/
-                // directory for faster rebuilds.
                 cacheDirectory: true,
-                // See #6846 for context on why cacheCompression is disabled
                 cacheCompression: false,
                 compact: isProd,
               },
             },
-            // Process any JS outside of the app with Babel.
-            // Unlike the application JS, we only compile the standard ES features.
-            {
-              test: /\.js$/,
-              exclude: /@babel(?:\/|\\{1,2})runtime/,
-              loader: require.resolve('babel-loader'),
-              options: {
-                babelrc: false,
-                configFile: false,
-                compact: false,
-                presets: [
-                  [
-                    require.resolve('babel-preset-react-app/dependencies'),
-                    { helpers: true },
-                  ],
-                ],
-                cacheDirectory: true,
-                // See #6846 for context on why cacheCompression is disabled
-                cacheCompression: false,
-
-                // Babel sourcemaps are needed for debugging into node_modules
-                // code.  Without the options below, debuggers like VSCode
-                // show incorrect code and set breakpoints on the wrong lines.
-                sourceMaps: isDev,
-                inputSourceMap: isDev,
-              },
-            },
-            // "postcss" loader applies autoprefixer to our CSS.
-            // "css" loader resolves paths in CSS and adds assets as dependencies.
-            // "style" loader turns CSS into JS modules that inject <style> tags.
-            // In production, we use MiniCSSExtractPlugin to extract that CSS
-            // to a file, but in development "style" loader enables hot editing
-            // of CSS.
-            // By default we support CSS Modules with the extension .module.css
             {
               test: cssRegex,
               exclude: cssModuleRegex,
@@ -218,29 +194,20 @@ module.exports = (webpackEnv) => {
               // See https://github.com/webpack/webpack/issues/6571
               sideEffects: true,
             },
-            // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
-            // using the extension .module.css
             {
               test: cssModuleRegex,
               use: getStyleLoaders({
                 importLoaders: 1,
                 sourceMap: isDev,
-                modules: {
+                modules: isDev ? {
                   localIdentName: '[name]_[local]',
+                } : {
+                  getLocalIdent: createLocalIdent(),
                 },
               }),
             },
-            // "file" loader makes sure those assets get served by WebpackDevServer.
-            // When you `import` an asset, you get its (virtual) filename.
-            // In production, they would get copied to the `build` folder.
-            // This loader doesn't use a "test" so it will catch all modules
-            // that fall through the other loaders.
             {
               loader: require.resolve('file-loader'),
-              // Exclude `js` files to keep "css" loader working as it injects
-              // its runtime that would otherwise be processed through "file" loader.
-              // Also exclude `html` and `json` extensions so they get processed
-              // by webpacks internal loaders.
               exclude: [/\.(js|ts|tsx)$/, /\.html$/, /\.json$/, /\.ejs$/],
               options: {
                 name: 'static/media/[name].[hash:4].[ext]',
