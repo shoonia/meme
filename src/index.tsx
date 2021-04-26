9b2dfbe078bab1fa85aeca10e72eb2552a139eae
shoonia <alexanderz@wix.com>
2021-04-26 18:15:26 +0300

    Setup store and router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { StoreContext } from 'storeon/react';
 
 import { App } from './components/App';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { getElementById } from './util';
+import { store } from './store';
 
 const root = getElementById('main');
 const header = getElementById('header');
@@ -12,9 +15,13 @@ const footer = getElementById('footer');
 
 render(
   <StrictMode>
-    <App />
-    <Header root={header} />
-    <Footer root={footer} />
+    <BrowserRouter>
+      <StoreContext.Provider value={store}>
+        <App />
+        <Header root={header} />
+        <Footer root={footer} />
+      </StoreContext.Provider>
+    </BrowserRouter>
   </StrictMode>,
   root,
 );
