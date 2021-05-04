ef54286286063a76b5fdb144b7a81ba2a3f153af
shoonia <alexanderz@wix.com>
2021-05-04 15:41:36 +0300

    Remove prefetch

--- src/components/NotFoundPage/index.tsx
+++ src/components/NotFoundPage/index.tsx
@@ -3,7 +3,6 @@ import { lazy, FC } from 'react';
 export const NotFoundPage: FC = lazy(() => {
   return import(
     /* webpackChunkName: "NotFoundPage" */
-    /* webpackPrefetch: true */
     './NotFoundPage'
   ).then((i) => {
     return {
