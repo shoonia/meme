90d4f4e0be1df1488c7745123c943dc504ad8d5b
shoonia <alexanderz@wix.com>
2021-05-06 23:20:39 +0300

    Add all data

--- src/components/List/Item.tsx
+++ src/components/List/Item.tsx
@@ -1,10 +1,29 @@
 import { FC } from 'react';
 
+import s from './Item.module.css';
 import { IItem } from '../../store/types';
 import { Image } from '../Image';
+import { Time } from '../Time';
 
-export const Item: FC<IItem> = ({ image }) => (
-  <figure>
-    <Image {...image} />
-  </figure>
-);
+export const Item: FC<IItem> = ({ title, body, date, image }) => {
+  const heading = title !== '' ? title : '***';
+
+  const figcaption = body !== '' && (
+    <figcaption>
+      {body}
+    </figcaption>
+  );
+
+  return (
+    <article>
+      <h2 className={s.title}>
+        {heading}
+      </h2>
+      <figure>
+        <Image {...image} />
+        {figcaption}
+      </figure>
+      <Time date={date} />
+    </article>
+  );
+};
